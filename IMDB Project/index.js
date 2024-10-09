6b9ecc3b28b3c23687a1ddeb28ac0ce959cbb80e
deoum <tayuhfavour@gmail.com>
2024-10-09 22:11:30 +0100

    was able to scrape the movie title from IMDB and display it on the browser

--- IMDB Project/index.js
+++ IMDB Project/index.js
@@ -1,30 +1,35 @@
-/* ============== THE INDEX.JS IS FOR THE BACKEND LOGIC  ========== */
-
-
-import express from "express";
-import cors from "cors";
-
-
-const port = 3000;
+import express from 'express';
+import cors from 'cors';
+import { searchMovies } from './scraper.js'; // Ensure this is correct
 const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Scraping is fun!',
+  });
+});
 
+// Updated route to use searchMovies dynamically
+app.get('/search/:title', async (req, res) => {
+  try {
+    const title = req.params.title;
+    const movies = await searchMovies(title); // Pass the title to searchMovies function
+    res.json(movies); // Respond with the list of movies
+  } catch (error) {
+    console.error('Error in /search/:title route:', error);
+    res.status(500).send('Error fetching movies');
+  }
+});
 
-
-
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
+// Optional: initial search to log movies on server start
+searchMovies().then((movies) => {
+  console.log('Movies found:', movies);
+});
--- IMDB Project/scraper.js
+++ IMDB Project/scraper.js
@@ -1,110 +1,33 @@
-/* ================     CODE TO SCRAP DATA FROM IMDB WEBSITE       ================ */
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
 
-import fetch from "node-fetch";
-import * as cheerio from "cheerio";
+export async function searchMovies(title) {
+  try {
+    console.log(`Searching movies with title: ${title}`);
 
+    // Adjust the URL to include the search title, using IMDb's search functionality
+    const searchUrl = `https://www.imdb.com/find?q=${encodeURIComponent(title)}&s=tt&ttype=ft&ref_=fn_ft`;
+    const response = await fetch(searchUrl);
+    const body = await response.text();
 
-
-const url = "https://www.imdb.com/find?s=tt&ttype=ft&ref_=fn_ft&q=";
-// const url = "https://www.imdb.com/find/?q=star%20wars&s=tt&ttype=ft&ref_=fn_ft";
-
-function searchMovies(searchTerm) {
-    return fetch(`${url}${searchTerm}`)
-    .then(response => response.text());
-}
-
-// website link. paste this link on your browser ====> "https://www.imdb.com/find?q=star%20wars&s=tt&ttype=ft&ref_=fn_ft"
-
-searchMovies('star wars')
-.then(body => {
     const $ = cheerio.load(body);
+    const movies = [];
 
-     //returns elements but skips as it goes
-     const movies = [];
-     $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each(function(i, element) {
-         const $element = $(element);
-         
-         $element.find('ul li').each(function() {
-             const $li = $(this);
-             const movie = {
-                 image: $li.find('img.ipc-image').attr('src'),
-                 title: $li.find('a.ipc-metadata-list-summary-item__t').text()
-             }
-             movies.push(movie);
-         });
-     }); 
-     console.log(movies);
+    // Adjust the selector to match IMDb's search results page
+    $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each((i, element) => {
+      const $element = $(element);
+      const movie = {
+        title: $element.find('a.ipc-metadata-list-summary-item__t').text(),
+        link: `https://www.imdb.com${$element.find('.result_text a').attr('href')}`,
+        image: $element.find('img.ipc-image').attr('src'),
+      };
+      movies.push(movie);
+    });
 
-})
+    return movies;
 
-
-
-//  =============== Bringing just one image link from the structure
-    // $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each(function(i, element) {
-    //     const $element = $(element);
-    //     // const $image = $element.find('div:nth-child(1) ul li div:nth-child(1) img');
-    //     const $image = $element.find('ul li img.ipc-image');
-    //     console.log($image.attr('src'));
-    // });
-
-     //  =============== using the .map method to retrieve all the image links, returns an ARRAY
-    /* $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each(function(i, element) {
-        const $element = $(element);
-        const $image = $element.find('ul li img.ipc-image');
-        const imageSources = $image.map(function() {
-            return $(this).attr('src');
-        }).get();
-        console.log(imageSources);  // Array of image sources
-    }); */
-
-        //  =============== using the FOR LOOP to retrieve all image links
-      /*  $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each(function(i, element) {
-            const $element = $(element);
-            const $image = $element.find('ul li img.ipc-image');
-            for(let i = 0; i < $image.length; i++){
-                console.log($image.eq(i).attr('src'));
-            }
-        }); */
-
-    //  =============== using the .each method to retrieve all the image links
-    /* $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each(function(i, element) {
-        const $element = $(element);
-        const $image = $element.find('ul li img.ipc-image');
-        $image.each(function() {
-            console.log($(this).attr('src'));
-        });
-    });  */
-
-
-    //  =============== using the .each method to retrieve all the movies
-    // const movies = [];
-    // $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa').each(function(i, element) {
-    //     const $element = $(element);
-    //     const $image = $element.find('ul li img.ipc-image');
-    //     const $title = $element.find('ul li a.ipc-metadata-list-summary-item__t');
-    //     $image.each(function() {
-    //         // console.log($(this).attr('src'));
-    //         $(this).attr('src');
-    //     });
-    //     const movie = {   
-    //     } 
-    // }); 
-
-
-   
-
-
-    
-    // Still skips return data
-    // const movies = [];
-    // $('section.ipc-page-section.ipc-page-section--base.sc-e8e4ce7-0.fkWWaa ul li').each(function(i, li) {
-    //     const $li = $(li);
-    //     const movie = {
-    //         image: $li.find('img.ipc-image').attr('src'),
-    //         title: $li.find('a.ipc-metadata-list-summary-item__t').text()
-    //     }
-    //     movies.push(movie);
-
-    // }); 
-    // console.log(movies);
-    
+  } catch (error) {
+    console.error('Error in searchMovies:', error);
+    throw error; // Propagate error to be caught in route handler
+  }
+}
